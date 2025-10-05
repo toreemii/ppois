b93fd78b67c2b031f420a0a6ace2c6b3f3cc1407
Вика <yavika635@gmail.com>
2025-10-05 15:37:33 +0300

    Delete lab1/task1/build directory

--- lab1/task1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vika/Рабочий стол/ppois/lab1/task1
-# Build directory: /home/vika/Рабочий стол/ppois/lab1/task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/home/vika/Рабочий стол/ppois/lab1/task1/build/tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/vika/Рабочий стол/ppois/lab1/task1/CMakeLists.txt;39;add_test;/home/vika/Рабочий стол/ppois/lab1/task1/CMakeLists.txt;0;")
