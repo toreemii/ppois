eb5c16f73eca2969d71c02c970a538b4698a102c
Вика <yavika635@gmail.com>
2025-10-05 16:04:50 +0300

    Update InputChecker.h

--- lab1/task1/InputChecker.h
+++ lab1/task1/InputChecker.h
@@ -6,5 +6,5 @@ class InputChecker
 public:
     static bool isNumericInput(int &number);
     static bool isInRange(int number, int min, int max);
-    static int inputInt(const std::string &message, int min = 1, int max = 7);
+    static int inputInt(const std::string &message, int min = 1, int max = 10);
 };
