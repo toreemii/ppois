01a277c94c76b74bf23d20abd7c192061a4cce42
Вика <yavika635@gmail.com>
2025-10-05 15:58:08 +0300

    Update and rename InputCheck.cpp to InputChecker.cpp

---
+++ lab1/task1/InputChecker.cpp
@@ -0,0 +1,40 @@
+#include "InputChecker.h"
+
+bool InputChecker::isNumericInput(int &number)
+{
+    std::cin >> number;
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cout << "Ошибка. Введите число.\n";
+        return false;
+    }
+    std::cin.ignore(1000, '\n');
+    return true;
+}
+
+bool InputChecker::isInRange(int number, int min, int max)
+{
+    if (number < min || number > max)
+    {
+        return false;
+    }
+    return true;
+}
+
+int InputChecker::inputInt(const std::string &message, int min, int max)
+{
+    int number;
+    while (true)
+    {
+        std::cout << message;
+
+        if (!isNumericInput(number))
+
+        if (!isInRange(number, min, max))
+            continue;
+
+        return number;
+    }
+}
